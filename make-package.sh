2bc6d33add53d183d30795e50a4491f49c007008
Jaehoon Choi <plaintext@andromedarabbit.net>
2022-02-16 13:26:45 +0900

    ElasticSearch 7.17.0

--- make-package.sh
+++ make-package.sh
@@ -8,7 +8,10 @@ elasticsearch_analysis=elasticsearch-analysis-mecab-ko
 
 # make Lucene/Solr package
 dir=mecab-ko-lucene-analyzer-$version
-mkdir $dir
+# if [[ -d $dir ]]; then
+# 	rm -rf $dir
+# fi
+mkdir -p $dir
 cp lucene-analyzer/target/$lucene_analyzer-$version-SNAPSHOT.jar $dir/$lucene_analyzer-$version.jar
 cp mecab-loader/target/$mecab_loader-$version-SNAPSHOT.jar $dir/$mecab_loader-$version.jar
 tar czf $dir.tar.gz $dir
@@ -16,7 +19,10 @@ rm -rf $dir
 
 # make ElasticSearch plugin
 dir=$elasticsearch_analysis-$version
-mkdir $dir
+# if [[ -d $dir ]]; then
+# 	rm -rf $dir
+# fi
+mkdir -p $dir
 cp lucene-analyzer/target/$lucene_analyzer-$version-SNAPSHOT.jar $dir/$lucene_analyzer-$version.jar
 cp mecab-loader/target/$mecab_loader-$version-SNAPSHOT.jar $dir/$mecab_loader-$version.jar
 cp elasticsearch-analysis-mecab-ko/target/$elasticsearch_analysis-$elasticsearch_plugin_version-SNAPSHOT.jar $dir/$elasticsearch_analysis-$elasticsearch_plugin_version.jar
